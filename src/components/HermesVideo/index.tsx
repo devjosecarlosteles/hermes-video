0806d7e48edec918ff300012b77c4d58f490d258
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-08-27 15:30:52 -0300

    context menu

--- src/components/HermesVideo/index.tsx
+++ src/components/HermesVideo/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import {
   Container
 } from "./style"
@@ -8,9 +8,32 @@ interface VideoContainer {
 }
 
 function VideoContainer ({ src }: VideoContainer) {
+  const [ showContextMenu, setShowContextMenu ] = useState(false)
+
+  function handleContextMenu(e: any) {
+    e.preventDefault() // block opening context menu
+
+    setShowContextMenu(true)
+  }
+
+  useEffect(() => {
+    window.addEventListener("click", () => {
+      setShowContextMenu(false)
+    })
+  }, [])
+
   return (
     <Container>
-      <video src={ src } controls={ false }></video>
+      <video 
+        src={ src } 
+        controls={ false }
+        onContextMenu={(e) => handleContextMenu(e)}
+      ></video>
+      {
+        showContextMenu ? (
+          "salve"
+        ) : null
+      }
     </Container>
   )
 }
