44ad35234418ec26885d7a9964e143332b9cc405
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-08-27 18:51:36 -0300

    timeline, resize div

--- src/components/HermesVideo/index.tsx
+++ src/components/HermesVideo/index.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react"
 import {
   Container
 } from "./style"
+import {
+  BsFillPlayFill
+} from "react-icons/bs"
 
 interface VideoContainer {
   src: string
@@ -10,15 +13,22 @@ interface VideoContainer {
 function VideoContainer ({ src }: VideoContainer) {
   const [ showContextMenu, setShowContextMenu ] = useState(false)
   const [ showControls, setShowControls ] = useState(false)
+  const [ showTimeline, setShowTimeline ] = useState(false)
 
   function handleContextMenu(e: any) {
     e.preventDefault() // block opening context menu
 
-    setShowContextMenu(true)
+    setShowContextMenu(true)  
   }
 
   function handleMouseMove() {
     setShowControls(true)
+    setShowTimeline(true)
+
+    setTimeout(() => {
+      setShowTimeline(false)
+      setShowControls(false)
+    }, 4000)
   }
 
   useEffect(() => {
@@ -33,8 +43,26 @@ function VideoContainer ({ src }: VideoContainer) {
         src={ src } 
         controls={ false }
         onContextMenu={ (e) => handleContextMenu(e) }
-        onMouseMove={ (e) => handleMouseMove() }
+        onMouseMove={ () => handleMouseMove() }
       ></video>
+      {
+        showTimeline ? (
+          <div id="timeline_container">
+            <div id="timeline"></div>
+          </div>
+        ) : null
+      }
+      {
+        showControls ? (
+          <div id="controls">
+            <BsFillPlayFill
+              className="control"
+              color="#FFF"
+              size={ 30 }
+            />
+          </div>
+        ) : null
+      }
     </Container>
   )
 }
--- src/components/HermesVideo/style.ts
+++ src/components/HermesVideo/style.ts
@@ -1,7 +1,42 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 600px;
+  height: 338px;
+
+  background-color: #000;
+
+  position: relative;
+
   video {
-    width: 600px;
+    width: 100%;
+    height: 100%;
+    
+    position: absolute;
+
+    top: 0;
+    left: 0;
+  }
+
+  #controls {
+    position: absolute;
+    
+    top: 40px;
+    left: 10px;
+
+    .control {
+      cursor: pointer;
+    }
+  }
+
+  #timeline_container {
+    width: 200px;
+    height: 10px;
+
+    background-color: red;
+
+    position: absolute;
+
+    bottom: 0;
   }
 `
